3f7616721e8f3e732c773085a14f961d525e3409
luchengLC <2050389814@qq.com>
2018-04-09 11:12:09 +0800

    完善页面

---
+++ appfront/src/components/Index/parts/TopBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div style="margin: 0; padding: 0">
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ff8080">
+      <el-menu-item class="el-menu-item" index="1">美妆商品导购系统</el-menu-item>
+      <el-menu-item class="el-menu-item" index="2">我的降价通知商品</el-menu-item>
+      <el-menu-item class="el-menu-item" id="logout" index="3">{{login_out}}</el-menu-item>
+      <el-menu-item class="el-menu-item" id="name-login" index="4">{{user_name}}</el-menu-item>
+
+    </el-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        user_name: '登录',
+        login_out: '注册',
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .el-menu {
+    .el-menu-item {
+      height: 30px;
+      line-height: 30px;
+    }
+
+    #name-login, #logout {
+      float: right;
+    }
+
+  }
+</style>
--- appfront/src/router/index.js
+++ appfront/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Main from '@/components/mainPage/index'
+import Index from '@/components/index/index'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/test',
       name: 'Test',
       component: Home
     },
