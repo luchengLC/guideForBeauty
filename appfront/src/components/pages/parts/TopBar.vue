2aef0947c0b2c15904512b6b2dd97e9ecded0fb2
luchengLC <2050389814@qq.com>
2018-04-16 13:17:00 +0800

    表单验证

--- appfront/src/components/pages/parts/TopBar.vue
+++ appfront/src/components/pages/parts/TopBar.vue
@@ -11,9 +11,9 @@
       text-color="#fff">
       <el-menu-item class="el-menu-item" index="/">美妆商品导购系统</el-menu-item>
       <el-menu-item class="el-menu-item" index="focus">我的降价通知商品</el-menu-item>
-      <el-button class="menu-btn" type="text" id="logout" @click="dialogRegisterVisible=true">{{register}}
+      <el-button class="menu-btn" type="text" id="logout" @click="dialogRegisterVisible=true">{{btn_register}}
       </el-button>
-      <el-button class="menu-btn" type="text" id="name-login" @click="dialogLoginVisible=true">{{login_out}}
+      <el-button class="menu-btn" type="text" id="name-login" @click="dialogLoginVisible=true">{{btn_login}}
       </el-button>
 
     </el-menu>
@@ -23,19 +23,19 @@
       title="登录"
       :visible.sync="dialogLoginVisible"
       width="500px"
-      :before-close="handleLoginClose">
-      <div class="el-dialog-div">
-        账号：
-        <el-input v-model="input_username" placeholder="手机号码..." style="width: 300px"></el-input>
-      </div>
-      <div class="el-dialog-div">
-        密码：
-        <el-input v-model="input_pw" placeholder="密码..." style="width: 300px"></el-input>
-      </div>
+      :before-close="handleLoginCansel">
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="90px">
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="loginForm.username" placeholder="手机号码..."></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" placeholder="密码..."></el-input>
+        </el-form-item>
+      </el-form>
 
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogLoginVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogLoginVisible = false">确 定</el-button>
+        <el-button @click="handleLoginCansel">取 消</el-button>
+        <el-button type="primary" @click="handleLoginSubmit">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -44,27 +44,26 @@
       title="注册"
       :visible.sync="dialogRegisterVisible"
       width="500px"
-      :before-close="handleRegisterClose">
-      <div class="el-dialog-div">
-        账 号：
-        <el-input v-model="input_username" placeholder="手机号码..." style="width: 300px"></el-input>
-      </div>
-      <div class="el-dialog-div">
-        昵 称：
-        <el-input v-model="input_name" placeholder="昵称..." style="width: 300px"></el-input>
-      </div>
-      <div class="el-dialog-div">
-        密 码：
-        <el-input v-model="input_pw" placeholder="密码..." style="width: 300px"></el-input>
-      </div>
-      <div class="el-dialog-div">
-        邮 箱：
-        <el-input v-model="input_email" placeholder="邮箱..." style="width: 300px"></el-input>
-      </div>
+      :before-close="handleRegisterCansel">
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="90px">
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="loginForm.username" placeholder="手机号码..."></el-input>
+        </el-form-item>
+        <el-form-item label="昵称" prop="name">
+          <el-input v-model="loginForm.name" placeholder="昵称..."></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" placeholder="密码..."></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="loginForm.email" placeholder="邮箱..."></el-input>
+        </el-form-item>
+
+      </el-form>
 
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogRegisterVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogRegisterVisible = false">确 定</el-button>
+        <el-button @click="handleRegisterCansel">取 消</el-button>
+        <el-button type="primary" @click="handleRegisterSubmit(loginForm)">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -80,36 +79,99 @@
     props: ["actives"],
     data () {
       return {
-        input_username: '',
-        input_name: '',
-        input_pw: '',
-        input_email: '',
         dialogLoginVisible: false,
         dialogRegisterVisible: false,
         msg: 'Welcome to Your Vue.js App',
-        register: '注册',
-        login_out: '登录',
+        btn_register: '注册',
+        btn_login: '登录',
         activeIndex: '',
+        loginForm: {
+          username: '',
+          password: '',
+          name: '',
+          email: '',
+        },
+        registerForm: {},
+        rules: {
+          username: [
+            {required: true, message: '请输入账号（手机号码）', trigger: 'blur'},
+            {pattern: /^0?(13[0-9]|[15[7-9]|153|156|18[7-9])[0-9]{8}$/, message: '请正确地输入账号（手机号码）', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'},
+            {min: 6, max: 30, message: '长度在 6 到 30 个字符', trigger: 'blur'},
+            {pattern: /^(\w){6,20}$/, message: '只能输入6-20个字母、数字、下划线', trigger: 'blur'}
+          ],
+          name: [
+            {required: true, message: '请输入昵称', trigger: 'blur'},
+          ],
+          email: [
+            {required: true, message: '请输入邮箱', trigger: 'blur'},
+            {
+              pattern: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$ /,
+              message: '请输入正确的邮箱地址',
+              trigger: 'blur'
+            }
+          ]
+        },
       }
     },
-    mounted: function() {
+    mounted: function () {
       this.$nextTick(function () {
         this.activeIndex = this.actives;
         console.log(this.activeIndex);
       })
     },
     methods: {
-      handleLoginClose(done) {
-        this.dialogLoginVisible = false
+      handleLoginCansel(done) {  //登录
+        this.dialogLoginVisible = false;
+        this.$refs['loginForm'].resetFields();  // 清空
 //        this.$confirm('确认关闭？')
 //          .then(_ => {
 //            done();
 //          })
 //          .catch(_ => {});
       },
+      handleRegisterCansel(done) {  // 注册
+        //
+        this.dialogRegisterVisible = false;
+        this.$refs['loginForm'].resetFields();  // 清空
+      },
+      handleLoginSubmit(){  //登录
 
-      handleRegisterClose(done) {
-        this.dialogRegisterVisible = false
+        // 处理
+        this.$refs['loginForm'].validate((valid) => {
+          if (valid) {
+            console.log('submit!!');
+
+            this.$http.post('http://127.0.0.1:8000/beauty/user/login', {
+              username: this.loginForm.username,
+              password: this.loginForm.password,
+            })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+
+
+            this.dialogLoginVisible = false;
+            this.$refs['loginForm'].resetFields();  // 清空
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+
+
+//        this.$refs['loginForm'].resetFields();  // 清空
+
+      },
+      handleRegisterSubmit(form){  // 注册
+        // 处理
+        this.dialogLoginVisible = false;
+        this.$refs['loginForm'].resetFields();  // 清空
       }
     },
 
