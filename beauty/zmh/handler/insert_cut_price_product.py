e142bb23b5659b448259458158baa18471fa8458
张敏华 <1578711560@qq.com>
2018-04-16 18:04:28 +0800

    增~降价通知接口

--- beauty/zmh/handler/insert_cut_price_product.py
+++ beauty/zmh/handler/insert_cut_price_product.py
@@ -8,6 +8,7 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'guideForBeauty.settings'
 django.setup()
 from beauty.models import UserCutPriceProduct
+from django.db.models import Q
 import  json
 # 检查用户手机号码格式是否正确
 def check_phone(user_phone):
@@ -29,13 +30,22 @@ def check_phone(user_phone):
 
 # 验证该用户是否重复点击同一个商品
 def checkAdd(product_json):
-    product = UserCutPriceProduct.objects.filter(Q(user_phone=product_json['user_phone'])&Q(product_address=product_json['item_url']))
-    if product is not None:
-        return True
-    else:
+    try:
+        product = UserCutPriceProduct.objects.filter(Q(user_phone=product_json['user_phone']),Q(product_address=product_json['item_url']))
+        product_list = list(product)
+        if product is None or product_list.__len__()==0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
         return False
 
-# 从数据库获取用户关注的商品列表
+
+# 添加用户关注的商品到数据库
+# 返回 0： 添加成功
+# 返回 1：添加失败，因为用户已经关注过该商品
+# 返回-1：添加失败，服务异常
 def add_product_to_table(product_json):
     try:
         if(checkAdd(product_json)):
@@ -47,14 +57,6 @@ def add_product_to_table(product_json):
                                           product_comment_count=product_json['comment_count'],
                                           product_platform = product_json['platform']
                                           )
-            #
-            # product.user_phone = product_json['user_phone']
-            # product.product_address = product_json['item_url']
-            # product.product_name = product_json['name']
-            # product.product_price = product_json['price']
-            # product.product_img_url = product_json['img_url']
-            # product.product_comment_count = product_json['comment_count']
-            # product.product_platform = product_json['platform']
             product.save()
             return 0
         else:
@@ -78,18 +80,21 @@ def handle_insert(request):
     # body = {'name': '32432432','user_phone': '12321332322323', 'platform': '13221', 'img_url': 'fdfsad', 'comment_count': 'dfdf', 'price': '12312', 'item_url': 'dsdSDS'}
     print(body)
     result = {}
-    if(check_phone(body['user_phone']) is False or add_product_to_table(body) ==-1):
+    result_code = add_product_to_table(body)
+    if(check_phone(body['user_phone']) is False or result_code ==-1):
         result['error_code'] = 1
-        result['msg'] = "服务器异常"
+        result['msg'] = "服务异常"
     else:
-        result['error_code'] = 0
-        result['msg'] = "success"
-    print (result)
+        if(result_code == 1):
+            result['error_code'] = 1
+            result['msg'] = "当前账号已经关注了该商品了，不需再次关注，请到“我的降价通知商品”页面查看！"
+        else:
+            result['error_code'] = 0
+            result['msg'] = "success"
+    print(result)
     return JsonResponse(result,safe=False)
 
 if __name__ == '__main__':
     # http://127.0.0.1:8000/beauty/productsList/getProductsPage?wd=卡姿兰蜗牛气垫调控霜&PageNo=1
     # keyword = '卡姿兰蜗牛气垫调控霜'
-    j = handle_insert('er')
-
-    print(j)
+    test = handle_insert('3')
